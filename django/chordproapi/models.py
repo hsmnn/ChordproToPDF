602ee03a83dfad3e29c3716ab925b242029f53ff
hsmnn <elie.hausmann@gmail.com>
2022-08-09 15:49:14 +0200

    insert dummy data

--- django/chordproapi/models.py
+++ django/chordproapi/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from datetime import date
 
-from django.contrib.auth.models import User
-
 # Create your models here.
 
 class Song(models.Model):
@@ -11,10 +9,10 @@ class Song(models.Model):
     author = models.CharField(max_length=100)
     base_key = models.CharField(max_length=2)
     tempo = models.IntegerField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    #created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
     created_on = models.DateField(default=date.today)
     last_updated = models.DateTimeField(default=date.today)
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="last_modifier")
+    #updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="last_modifier")
     content = models.TextField()
 
     class Meta:
--- django/chordproapi/serializers.py
+++ django/chordproapi/serializers.py
@@ -4,4 +4,4 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ('title', 'author', 'base_key', 'tempo', 'created_by', 'created_on', 'last_updated', 'updated_by', 'content')
+        fields = ('title', 'author', 'base_key', 'tempo', 'created_on', 'last_updated', 'content')
